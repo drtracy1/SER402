fba1892a25ec67433b0e75c69996cccfdbafe6b4
klicata <klicata@asu.edu>
2016-02-10 07:39:15 -0700

    Get info from user to create account
    Information provided by the user is now into a variable.

--- WeAllCode/app/src/main/java/ser402team/weallcode/CreateAccActivity.java
+++ WeAllCode/app/src/main/java/ser402team/weallcode/CreateAccActivity.java
@@ -2,6 +2,10 @@ package ser402team.weallcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateAccActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class CreateAccActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acc);
+
+        Button createAccountButt = (Button) findViewById(R.id.buttonDone);
+
+        createAccountButt.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+                        //get username, password and email from user
+                        final EditText newName = (EditText) findViewById(R.id.editName);
+                        final EditText newPwd = (EditText) findViewById(R.id.editPassword);
+                        final EditText newEmail = (EditText) findViewById(R.id.editEmail);
+                        String strNewName = newName.getText().toString();
+                        String strPassword = newPwd.getText().toString();
+                        String strEmail = newEmail.getText().toString();
+
+                        Toast.makeText(getApplicationContext(), strNewName + " created",
+                                    Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
     }
 }

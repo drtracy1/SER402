440b121d69975ef26b47872bf2fb77efd240d5d8
klicata <klicata@asu.edu>
2016-02-12 13:46:54 -0700

    Create account, userInfo class added
    A user creates a unique account then goes to main menu. Accounts are
    added temporary into a hash map.

--- WeAllCode/app/src/main/java/ser402team/weallcode/CreateAccActivity.java
+++ WeAllCode/app/src/main/java/ser402team/weallcode/CreateAccActivity.java
@@ -1,5 +1,6 @@
 package ser402team.weallcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,7 +8,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class CreateAccActivity extends AppCompatActivity {
+
+    public final static String USERNAME = "ser402team.weallcode.USERNAME";
+
+    //FUTURE NOTE: implement serialization to forward hashmap with intent
+    HashMap<String, Object> allUsers = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +35,42 @@ public class CreateAccActivity extends AppCompatActivity {
                         String strPassword = newPassword.getText().toString();
                         String strEmail = newEmail.getText().toString();
 
-                        Toast.makeText(getApplicationContext(), strUsername + " created",
+                        //check for already used username
+                        if(!usernameExists(strUsername)){
+                            usersInformation newUser = new usersInformation(strUsername, strPassword, strEmail);
+                            allUsers.put(strUsername, newUser);
+                            allowLogin(strUsername);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), strUsername + " already exists",
                                     Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
         );
 
+    }
+
+    private boolean usernameExists(String strUsr) {
+        //if the hashmap is empty then username does not exsits yet
+        if(allUsers.isEmpty()) {
+            return false;
+        }
+
+        //go through hashmap to find if username exists
+        for(int i = 0; i < allUsers.size(); i++) {
+            if (allUsers.containsKey(strUsr)) {
+                //username exists
+                return true;
+            }
+        }
+        //username does not exist
+        return false;
+    }
+
+    private void allowLogin(String userName) {
+        Intent intent = new Intent(CreateAccActivity.this, MainPageActivity.class);
+        intent.putExtra(USERNAME, userName);
+        startActivity(intent);
     }
 }

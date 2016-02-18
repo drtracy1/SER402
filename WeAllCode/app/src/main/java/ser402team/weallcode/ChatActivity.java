f5b570dc2fc7efc7a5618b1741897741d3d0b09f
drtracy1 <drtracy1@asu.edu>
2016-02-18 00:36:58 -0700

    Added the chat UI

---
+++ WeAllCode/app/src/main/java/ser402team/weallcode/ChatActivity.java
@@ -0,0 +1,13 @@
+package ser402team.weallcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}

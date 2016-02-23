305525e66c6e7815e53c74f2cc94a98157e23292
klicata <klicata@asu.edu>
2016-02-23 12:03:48 -0700

    User class
    User class has user’s characteristics.

---
+++ WeAllCode/app/src/main/java/ser402team/weallcode/UserInformation.java
@@ -0,0 +1,57 @@
+package ser402team.weallcode;
+
+/**
+ * Created by KBasra on 2/23/16.
+ */
+public class UserInformation {
+
+    private String username = "";
+    private String password = "";
+    private String email = "";
+    private int points = 0;
+
+    UserInformation() {
+
+    }
+
+    UserInformation(String un, String em, String pw) {
+        this.username = un;
+        this.password = pw;
+        this.email = em;
+    }
+
+    public void setPassword(String pw) {
+        this.password = pw;
+    }
+
+    public void setEmail(String em) {
+        this.email = em;
+
+    }
+
+    public void addPoints(int num) {
+        this.points += num;
+
+    }
+
+    public void setUsername(String un) {
+        this.username = un;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public int getPoints() {
+        return this.points;
+    }
+}

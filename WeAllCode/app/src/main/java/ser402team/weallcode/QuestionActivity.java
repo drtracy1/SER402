70264da6b2e6ef9be6dc1042b5890913c61c5276
klicata <klicata@asu.edu>
2016-03-02 06:10:37 -0700

    Retrieve QandA from db
    The questions and answers can now come from the database.

--- WeAllCode/app/src/main/java/ser402team/weallcode/QuestionActivity.java
+++ WeAllCode/app/src/main/java/ser402team/weallcode/QuestionActivity.java
@@ -11,6 +11,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,18 +28,23 @@ import java.util.Random;
 
 /*
  *@author daniel tracy
- * Timer by Kristel
+ * Timer and retrieve DB data methods by Kristel
+ * Timer updated by Crystal
  */
 
 public class QuestionActivity extends AppCompatActivity {
 
-     private ArrayList<QuestionAnswer> questionList = new ArrayList<QuestionAnswer>();
-     private boolean[] isUsed;
+    private ArrayList<QuestionAnswer> questionList = new ArrayList<QuestionAnswer>();
+    private boolean[] isUsed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        //connect to firebase
+        Firebase.setAndroidContext(this);
+        final Firebase REF = new Firebase("https://weallcode-questions.firebaseio.com/");
 
         //add timer text
         startTimer();
@@ -56,7 +71,8 @@ public class QuestionActivity extends AppCompatActivity {
         });
 
         //Generate question list
-        generateQAList();
+        generateQAList(REF);
+
         //Create an isUsed list to determine which questions have been used
         isUsed = new boolean[questionList.size()];
         Arrays.fill(isUsed, Boolean.FALSE);
@@ -67,20 +83,32 @@ public class QuestionActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 int index = 0;
+
+                /* Firebase has been a bit of a pain, for some reason android would not complete
+                 * generateQAList method before it sets up the isUsed, so it was always empty  */
+                if(isUsed.length == 0) {
+                    //Create an isUsed list to determine which questions have been used
+                    isUsed = new boolean[questionList.size()];
+                    Arrays.fill(isUsed, Boolean.FALSE);
+                }
+
                 //Check to see if all the questions have been used or not
-                if(isAllTrue(isUsed) == false) {
-                    index = genRandInt();
-                    while (isUsed[index] == true) {
-                        index = genRandInt();
+                if(isAllTrue(isUsed) == false) {    //if not all the questions have been touched
+                    index = genRandInt();           //get random element number
+                    while (isUsed[index] == true) { //if that random number is already used
+                        index = genRandInt();       //get another
                     }
                 }
+
                 TextView questionText = (TextView)findViewById(R.id.questionTextField);
-                if(isAllTrue(isUsed) == false) {
-                    questionText.setText(questionList.get(index).getQuestion());
-                } else {
+
+                if(isAllTrue(isUsed) == false) {     //if not all questions have been used
+                    questionText.setText(questionList.get(index).getQuestion()); //get index to question from last if block
+                } else {                            //all questions have been used
                     questionText.setText("End of Questions");
                 }
                 isUsed[index] = true;
+
                 //resets the timer for new question.
                 startTimer();
             }
@@ -89,7 +117,14 @@ public class QuestionActivity extends AppCompatActivity {
     }
 
     //Here you should populate the QA list
-    protected void generateQAList() {
+    protected void generateQAList(Firebase REF) {
+
+        //set 5 questions to the list --> this logic can be changed later
+        for(int i = 0; i < 5; i++) {
+            setQuestion(REF);
+        }
+
+        /*
         QuestionAnswer qA1 = new QuestionAnswer("1 kilobyte is equal to: ", "1024 bytes", "1000 bytes, duh", "1064 bytes");
         QuestionAnswer qA2 = new QuestionAnswer("What do you use to repeat segments of code?", "Loops", "Rings", "Repeaters");
         QuestionAnswer qA3 = new QuestionAnswer("What do strings represent?", "Character sequences", "Cotton", "Integers");
@@ -105,6 +140,7 @@ public class QuestionActivity extends AppCompatActivity {
         addQuestionToList(qA5);
         addQuestionToList(qA6);
         addQuestionToList(qA7);
+        */
     }
 
     //Add the string to the list
@@ -117,6 +153,26 @@ public class QuestionActivity extends AppCompatActivity {
         return num;
     }
 
+    //Use the size of the question list to return a random integer index
+    protected String getRandomInt(long max) {
+        Random rand = new Random();
+        int maxQuestions = 0;
+
+        try {
+            maxQuestions = (int) max;
+            maxQuestions -= 1; //account for 0 but not for the number of questions
+            maxQuestions = rand.nextInt(maxQuestions);
+        } catch (ArithmeticException ae) {
+            //just in case there is an overflow from long to int, get random number
+            maxQuestions = rand.nextInt(11); //<-- need to change that number eventually
+        }
+        //if there is no children in the firebase db --> send an error message
+        if(maxQuestions <0) { return "Error"; }
+
+        //turn into string for easy search in firebase db
+        return Integer.toString(maxQuestions);
+    }
+
     //Check if the boolean array is all true or not
     protected boolean isAllTrue(boolean[] arr) {
         for(int i=0; i<arr.length; i++) {
@@ -125,6 +181,57 @@ public class QuestionActivity extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    protected void setQuestion(Firebase REF) {
+        //get random question and answer from firebase
+        REF.child("questionAnswer").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                long maxQuestions = dataSnapshot.getChildrenCount();
+                //System.out.println("NUMBER OF QUESTIONS: "+maxQuestions);
+                String randomNumberQuestion = getRandomInt(maxQuestions);
+
+                //no error coming from random generator and answer/question set found
+                if (!randomNumberQuestion.equalsIgnoreCase("Error") || dataSnapshot.child(randomNumberQuestion).getValue() != null) {
+                    //get value, turn it into JSONObject, then separate values into string
+                    String str = dataSnapshot.child(randomNumberQuestion).getValue().toString();
+
+                    try {
+                        //make sure the string is seen as a JSONObject
+                        Object json = new JSONTokener(str).nextValue();
+                        if(json instanceof JSONObject) {
+                            JSONObject jObj = new JSONObject(str);
+
+                            String question = jObj.get("Question").toString();
+                            String answer = jObj.get("Answer").toString();
+                            String wrongAnswer1 = jObj.get("Wrong1").toString();
+                            String wrongAnswer2 = jObj.get("Wrong2").toString();
+
+                            setAnswerQuestion(question, answer, wrongAnswer1, wrongAnswer2);
+                        }
+                        //typically caused by randomNumberQuestion, but it is fixed (keeping just in case)
+                        else {
+                            System.out.println("Something went wrong");
+                        }
+
+                    } catch (JSONException jex) {
+                        jex.printStackTrace();
+                    }
+                }
+                //question not found or error in random number generator
+                else {
+                    Toast.makeText(getApplicationContext(), "Error retrieving question",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("There was an error");
+            }
+        });
     }
 
     protected void startTimer() {
@@ -154,5 +261,10 @@ public class QuestionActivity extends AppCompatActivity {
 
             }
         }.start();
+    }
+
+    protected void setAnswerQuestion(String q, String a, String w1, String w2) {
+        QuestionAnswer qa = new QuestionAnswer(q, a, w1, w2);
+        addQuestionToList(qa);
     }
 }
